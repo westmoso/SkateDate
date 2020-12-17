4a6e991560b161fc52f9203b1d9916e9287b18d4
westmoso <westmoso@gmail.com>
2020-12-16 20:36:16 -0500

    creating pages to test signup and signin.

--- my-app/server/models/skater.js
+++ my-app/server/models/skater.js
@@ -88,7 +88,7 @@ function validateSkater(Skater) {
     const schema = Joi.object({
         username: Joi.string().min(5).max(50).required(),
         email: Joi.string().min(5).max(255).required().email(),
-        password: Joi.string().min(5).max(1024).required(path)
+        password: Joi.string().min(5).max(1024).required()
     });
     return schema.validate(Skater);
 }
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import './App.css';
 import Sidebar from "./Components/Sidebar"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SignUp from "./Pages/SignUp";
+import SignIn from "./Pages/SignIn";
 import SkaterCard from "./Components/SkaterCard";
 
 
@@ -13,6 +15,8 @@ function App() {
         <Router>
           <Sidebar />
           <Switch>
+            <SignUp path="/SignUp" />
+            <SignIn path="/SignIn" />
             <Route path='/'>
               <SkaterCard />
             </Route>
