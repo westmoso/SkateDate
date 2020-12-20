492224d10584129b1dd5406b72ad53613eedb59f
westmoso <westmoso@gmail.com>
2020-12-19 22:18:12 -0500

    receiving object key error on signup page.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,25 +5,23 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import SignUp from "./Pages/SignUp";
 import SignIn from "./Pages/SignIn";
 import SkaterCard from "./Components/SkaterCard";
-import SkaterProvider from "./hooks/useSkater";
+
 
 
 export default function App() {
   return (
     <>
       <div className="App">
-        <SkaterProvider>
-          <Router>
-            <Sidebar />
-            <Switch>
-              <SignUp path="/SignUp" />
-              <SignIn path="/SignIn" />
-              <Route path='/'>
-                <SkaterCard />
-              </Route>
-            </Switch>
-          </Router>
-        </SkaterProvider>
+        <Router>
+          <Sidebar />
+          <Switch>
+            <SignUp path="/SignUp" />
+            <SignIn path="/SignIn" />
+            <Route path='/'>
+              <SkaterCard />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </>
   );
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import reportWebVitals from './reportWebVitals';
-import 'core-js';
 
 ReactDOM.render(
   <React.StrictMode>
