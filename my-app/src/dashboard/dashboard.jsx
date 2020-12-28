793dfbe4d50ade4ff20e85624bf75ac7e0e8fd76
westmoso <westmoso@gmail.com>
2020-12-28 16:55:02 -0500

    coded axios call to retrieve skaters.

--- my-app/src/dashboard/dashboard.jsx
+++ my-app/src/dashboard/dashboard.jsx
@@ -7,7 +7,7 @@ import SkaterCard from '../components/card/SkaterCard';
 import openSocket from 'socket.io-client';
 
 
-function Dashboard() {
+export default class Dashboard extends React.Component() {
     constructor(props){
         super(props);
         this.state={
@@ -16,11 +16,10 @@ function Dashboard() {
             message: '',
             url: '',
             loggedSkater:{},
-            blocked: []
         }
-        this.loggedSkater={blocked: []};
         this.socket={};
     }
+
     openModal() {
         this.setState({
             visible : true
@@ -32,14 +31,15 @@ function Dashboard() {
             visible : false
         });
     }
-    
-componentDidMount(){
+
+
+    componentDidMount(){
         this.loggedSkater=this.props.location.state[0];
         this.setState({
             loggedSkater: this.loggedSkater
         })
         console.log('loggedSkater', this.loggedSkater);
-        this.socket=openSocket('http://localhost:1234');
+        this.socket=openSocket('http://localhost:3000');
         console.log('socket id test', this.socket.id);
         this.socket.on('connection',()=>{
             console.log('socket id', this.socket.id);
@@ -51,8 +51,8 @@ componentDidMount(){
             
         });
 
-         this.socket.on('sendLike',(data)=>{
-            console.log('sendLike event called  on frontend..', data);
+        this.socket.on('sendLike',(data)=>{
+            console.log('sent like on frontend..', data);
             
             this.setState({
                 message: data.message,
@@ -60,6 +60,46 @@ componentDidMount(){
             });
             this.openModal();
         });
+        
+
+
+    Axios('http://localhost:3000/api/getSkaters',{
+                method:'GET',
+                headers:{
+                    'Content-Type':'application/json',
+                    'authorization': `Bearer ${localStorage.getItem('token')}`
+            },
+            credentials: 'include',
+            withCredentials: true
+            }).then(data=>{
+                console.log('data', data.data);
+                if(data.data.status==403){
+                    console.log('unauth skater, redirecting..');
+                    this.props.history.push('/');
+                    localStorage.clear();
+                }
+                    else{
+                        this.loggedSkater=data.data.content.filter(skater=>this.state.loggedSkater.email===skater.email)[0];
+                        console.log('updated loggedSkater details', this.loggedSkater);
+                        this.setState({
+                            loggedSkater: this.loggedSkater
+                        });
+
+                        data.data.content=data.data.content.filter(skaterObject=>skaterObject.email!=this.loggedSkater.email);
+                        console.log('modified data.data.content', data.data.content);
+                        data.data.content.forEach(skater=>{
+                                    
+                        });
+                        console.log('blocked array', this.state.blocked);
+                        this.setState({
+                            skaters: data.data.content,
+                            blocked: this.state.blocked
+                        });
+                        console.log('skaters', this.state.skaters);
+      
+                    }
+            });
+    }
 
     return (
         <div>
@@ -67,5 +107,3 @@ componentDidMount(){
         </div>
     )
 }
-
-export default Dashboard
