b2c5b37a54b1ed8a4ff0419e62b1044886200638
westmoso <westmoso@gmail.com>
2020-12-28 15:12:08 -0500

    deleted component and styling folders for file restructuring.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,11 @@
 import React from "react";
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import SignUp from "./Pages/SignUp";
-import SignIn from "./Pages/SignIn";
-import SkaterCard from "./Components/SkaterCard";
-import Header from "./Components/Header"
-import SwipeButtons from "./Components/SwipeButtons"
-
 
 export default function App() {
   return (
     <>
       <div className="App">
-        <Router>
-          <Header />
-          <Switch>
-            <SignUp path="/SignUp" />
-            <SignIn path="/SignIn" />
-            <Route path='/'>
-              <SkaterCard />
-            </Route>
-          </Switch>
-        </Router>
-        <SwipeButtons />
+        test
       </div>
     </>
   );
---
+++ my-app/src/dashboard/dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Axios from 'axios';
+import './dashboard.css';
+import Modal from 'react-awesome-modal';
+
+
+function dashboard() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default dashboard
