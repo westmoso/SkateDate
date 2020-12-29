942e1ebc07404d8d057b6ece6d6cd82fc510b286
westmoso <westmoso@gmail.com>
2020-12-29 12:23:08 -0500

    this is so fucking tedious!

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,12 @@
 import React from "react";
 import './App.css';
-import Dashboard from './dashboard/Dashboard';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import SocketProvider from 'react-socket.io';
-import openSocket from 'socket.io-client';
-
-const socket = openSocket('http://localhost:3000');
 
 export default function App() {
   return (
-    <SocketProvider socket={socket}>
-      <div className="App">
-        <BrowserRouter>
-          <Switch>
-            <Route path="/dashboard" exact component={Dashboard} />
-          </Switch>
-        </BrowserRouter>
-      </div>
-    </SocketProvider>
+
+    <div className="App">
+      test
+    </div>
   );
 }
 
